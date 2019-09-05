d11e868abb22208f56096e14f287f78e978033d2
Lakshay Garg <lakshaygarg.in@gmail.com>
2019-09-05 13:38:26 +0000

    QCamera2: [-Werror,-Wtautological-pointer-compare]
    Signed-off-by: Lakshay Garg <lakshaygarg.in@gmail.com>

--- camera/QCamera2/Android.mk
+++ camera/QCamera2/Android.mk
@@ -35,7 +35,7 @@ LOCAL_SRC_FILES += \
         HAL/QCameraParameters.cpp \
         HAL/QCameraThermalAdapter.cpp
 
-LOCAL_CFLAGS := -Wall -Wextra -Werror
+LOCAL_CFLAGS := -Wall -Wextra -Werror -Wno-error -Wno-tautological-pointer-compare
 LOCAL_CFLAGS += -DHAS_MULTIMEDIA_HINTS
 LOCAL_CFLAGS += -DENABLE_MODEL_INFO_EXIF
 
