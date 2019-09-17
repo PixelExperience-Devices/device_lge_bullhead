f493d870d56a0fc7b256d6e57c030cf9a786b62a
Lakshay <lakshaygarg.in@gmail.com>
2019-09-17 17:55:27 +0000

    Audio: Do not build Audio 5.0 HAL
    Signed-off-by: Lakshay <lakshaygarg.in@gmail.com>

--- device.mk
+++ device.mk
@@ -245,12 +245,10 @@ PRODUCT_PACKAGES += \
     rmnetcli
 
 PRODUCT_PACKAGES += \
-    android.hardware.audio@5.0-impl \
     android.hardware.audio@2.0-service \
-    android.hardware.audio.effect@5.0-impl \
-    android.hardware.soundtrigger@2.0-service \
+    android.hardware.audio@2.0-impl \
     android.hardware.soundtrigger@2.0-impl \
-    android.hardware.audio.effect@2.0-service
+    android.hardware.audio.effect@2.0-impl
 
 PRODUCT_PACKAGES += \
     android.hardware.graphics.composer@2.1-impl \
