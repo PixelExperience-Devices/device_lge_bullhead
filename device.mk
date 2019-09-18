1c79130d4fed8279df1ecbcb37525426c90a7117
Lakshay <lakshaygarg.in@gmail.com>
2019-09-18 19:26:39 +0000

    Binderize GNSS HAL
    Signed-off-by: Lakshay <lakshaygarg.in@gmail.com>

--- device.mk
+++ device.mk
@@ -556,7 +556,8 @@ PRODUCT_PACKAGES += \
 
 #GNSS HAL
 PRODUCT_PACKAGES += \
-    android.hardware.gnss@1.0-impl
+    android.hardware.gnss@1.0-impl \
+    android.hardware.gnss@1.0-service
 
 #USB HAL
 PRODUCT_PACKAGES += \
