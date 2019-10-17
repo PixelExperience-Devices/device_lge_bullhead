4ca2d52bf3444014a962a733cfceaa1f7cb1b424
Lakshay <lakshaygarg.in@gmail.com>
2019-10-17 08:56:20 +0000

    Bullhead: Build Thermal HAL
    Signed-off-by: Lakshay <lakshaygarg.in@gmail.com>

--- device.mk
+++ device.mk
@@ -553,9 +553,8 @@ PRODUCT_PACKAGES += \
     android.hardware.power@1.0-impl \
 
 # Thermal HAL
-#PRODUCT_PACKAGES += \
-#    thermal.bullhead \
-#    android.hardware.thermal@1.0-impl \
+PRODUCT_PACKAGES += \
+    thermal.bullhead 
 
 #GNSS HAL
 PRODUCT_PACKAGES += \
