7a76f5bc942764879b8c5b31d6d7679e0d9bc936
Lakshay <lakshaygarg.in@gmail.com>
2019-11-10 14:43:40 +0000

    Revert "rootdir: init: general improvements"
    This reverts commit 2aa4f2d3a77857591bc51ab7ef7f0129a3e774c4.

--- init.bullhead.power.sh
+++ init.bullhead.power.sh
@@ -18,13 +18,6 @@ function get-set-forall() {
     done
 }
 
-function writepid_sbg() {
-    until [ ! "$1" ]; do
-        echo -n $1 > /dev/cpuset/system-background/tasks
-        shift
-    done
-}
-
 ################################################################################
 
 # take the A57s offline when thermal hotplug is disabled
@@ -134,54 +127,3 @@ get-set-forall /sys/devices/soc.0/qcom,bcl.*/mode enable
 
 # set GPU default power level to 5 (180MHz) instead of 4 (305MHz)
 write /sys/class/kgsl/kgsl-3d0/default_pwrlevel 5
-
-# devfreq
-get-set-forall /sys/class/devfreq/qcom,cpubw*/governor bw_hwmon
-restorecon -R /sys/class/devfreq/qcom,cpubw*
-get-set-forall /sys/class/devfreq/qcom,cpubw*/bw_hwmon/sample_ms 4
-get-set-forall /sys/class/devfreq/qcom,cpubw*/bw_hwmon/io_percent 34
-get-set-forall /sys/class/devfreq/qcom,cpubw*/bw_hwmon/hist_memory 20
-get-set-forall /sys/class/devfreq/qcom,cpubw*/bw_hwmon/hyst_length 10
-get-set-forall /sys/class/devfreq/qcom,cpubw*/bw_hwmon/low_power_ceil_mbps 0
-get-set-forall /sys/class/devfreq/qcom,cpubw*/bw_hwmon/low_power_io_percent 34
-get-set-forall /sys/class/devfreq/qcom,cpubw*/bw_hwmon/low_power_delay 20
-get-set-forall /sys/class/devfreq/qcom,cpubw*/bw_hwmon/guard_band_mbps 0
-get-set-forall /sys/class/devfreq/qcom,cpubw*/bw_hwmon/up_scale 250
-get-set-forall /sys/class/devfreq/qcom,cpubw*/bw_hwmon/idle_mbps 1600
-get-set-forall /sys/class/devfreq/qcom,mincpubw*/governor cpufreq
-
-{
-
-sleep 10
-
-LOGCAT=`pidof logcat`
-RMT_STORAGE=`pidof rmt_storage`
-QMUXD=`pidof qmuxd`
-QTI=`pidof qti`
-NETMGRD=`pidof netmgrd`
-THERMAL-ENGINE=`pidof thermal-engine`
-WPA_SUPPLICANT=`pidof wpa_supplicant`
-LOC_LAUNCHER=`pidof loc_launcher`
-CNSS-DAEMON=`pidof cnss-daemon`
-QSEECOMD=`pidof qseecomd`
-TIME_DAEMON=`pidof time_daemon`
-CND=`pidof cnd`
-IMSQMIDAEMON=`pidof imsqmidaemon`
-IMSDATADAEMON=`pidof imsdatadaemon`
-
-writepid_sbg $LOGCAT
-writepid_sbg $RMT_STORAGE
-writepid_sbg $QMUXD
-writepid_sbg $QTI
-writepid_sbg $NETMGRD
-writepid_sbg $THERMAL-ENGINE
-writepid_sbg $WPA_SUPPLICANT
-writepid_sbg $LOC_LAUNCHER
-writepid_sbg $CNSS-DAEMON
-writepid_sbg $QSEECOMD
-writepid_sbg $TIME_DAEMON
-writepid_sbg $CND
-writepid_sbg $IMSQMIDAEMON
-writepid_sbg $IMSDATADAEMON
-
-}&
