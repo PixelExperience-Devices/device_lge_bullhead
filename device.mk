3776b7c15367784623d31b4b270806069a859868
Lakshay Garg <lakshaygarg.in@gmail.com>
2020-03-22 06:47:13 +0000

    Revert "Bullhead: Power: Use QTI Powerhal 1.2"
    This reverts commit 665c6a610bb6fe21d6307932c61e22720925319f & 34f8eb89f7cb0b7a057f1702e62be7fd0cb7343a.

--- device.mk
+++ device.mk
@@ -562,7 +562,8 @@ PRODUCT_PROPERTY_OVERRIDES += \
 
 # Power HAL
 PRODUCT_PACKAGES += \
-android.hardware.power@1.2-service.bullhead
+    power.bullhead \
+    android.hardware.power@1.0-impl \
 
 # Thermal HAL
 PRODUCT_PACKAGES += \
--- power/Android.mk
+++ power/Android.mk
@@ -1,66 +1,28 @@
 LOCAL_PATH := $(call my-dir)
 
+# HAL module implemenation stored in
+# hw/<POWERS_HARDWARE_MODULE_ID>.<ro.hardware>.so
 include $(CLEAR_VARS)
 
 LOCAL_MODULE_RELATIVE_PATH := hw
+LOCAL_MULTILIB := both
+LOCAL_SHARED_LIBRARIES := liblog libcutils libdl
+LOCAL_SRC_FILES := power.c metadata-parser.c utils.c list.c hint-data.c
 
-LOCAL_SHARED_LIBRARIES := \
-    liblog \
-    libcutils \
-    libdl \
-    libbase \
-    libhidlbase \
-    libhidltransport \
-    libhwbinder \
-    libutils \
-    android.hardware.power@1.2 
+# Include target-specific files.
 
-LOCAL_HEADER_LIBRARIES := \
-    libhardware_headers
-
-LOCAL_SRC_FILES := \
-    power-common.c \
-    metadata-parser.c \
-    utils.c \
-    list.c \
-    hint-data.c \
-    service.cpp \
-    Power.cpp
-
-LOCAL_CFLAGS += -Wall -Wextra -Werror
-
-ifeq ($(call is-board-platform-in-list,msm8992), true)
-LOCAL_SRC_FILES += power-8992.c
-endif
-
-ifeq ($(call is-board-platform-in-list,msm8994), true)
+ifeq ($(call is-board-platform-in-list, msm8994), true)
 LOCAL_SRC_FILES += power-8994.c
 endif
 
-ifneq ($(TARGET_POWER_SET_FEATURE_LIB),)
-    LOCAL_STATIC_LIBRARIES += $(TARGET_POWER_SET_FEATURE_LIB)
-endif
-
-ifneq ($(TARGET_POWERHAL_SET_INTERACTIVE_EXT),)
-    LOCAL_CFLAGS += -DSET_INTERACTIVE_EXT
-    LOCAL_SRC_FILES += ../../../../$(TARGET_POWERHAL_SET_INTERACTIVE_EXT)
-endif
-
-ifneq ($(TARGET_TAP_TO_WAKE_NODE),)
-    LOCAL_CFLAGS += -DTAP_TO_WAKE_NODE=\"$(TARGET_TAP_TO_WAKE_NODE)\"
+ifeq ($(call is-board-platform-in-list, msm8992), true)
+LOCAL_SRC_FILES += power-8992.c
 endif
 
 ifeq ($(TARGET_USES_INTERACTION_BOOST),true)
     LOCAL_CFLAGS += -DINTERACTION_BOOST
 endif
 
-ifeq ($(TARGET_ARCH),arm)
-    LOCAL_CFLAGS += -DARCH_ARM_32
-endif
-
-LOCAL_MODULE := android.hardware.power@1.2-service.bullhead
-LOCAL_INIT_RC := android.hardware.power@1.2-service.bullhead.rc
+LOCAL_MODULE := power.bullhead
 LOCAL_MODULE_TAGS := optional
-LOCAL_VENDOR_MODULE := true
-
-include $(BUILD_EXECUTABLE)
+include $(BUILD_SHARED_LIBRARY)
