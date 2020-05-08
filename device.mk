92f4776bbd1460f5e36959c39d29e531637204b6
Nirmal Abraham <nirmalabraham@gmail.com>
2020-05-08 08:19:48 +0000

    Bullhead: Disable SF Latch Buffer Signaled property
    Signed-off-by: Lakshay Garg <lakshaygarg.in@gmail.com>

--- device.mk
+++ device.mk
@@ -638,7 +638,9 @@ PRODUCT_DEFAULT_PROPERTY_OVERRIDES += \
 PRODUCT_PROPERTY_OVERRIDES += \
     persist.sys.disable_rescue=true
 
+# Graphics
 PRODUCT_DEFAULT_PROPERTY_OVERRIDES += \
+    debug.sf.latch_unsignaled=1 \
     ro.surface_flinger.force_hwc_copy_for_virtual_displays=true \
     ro.surface_flinger.max_frame_buffer_acquired_buffers=3 \
     ro.surface_flinger.max_virtual_display_dimension=2048 \
