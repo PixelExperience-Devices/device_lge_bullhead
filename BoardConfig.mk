210b463833ababd183749f34951048f91c605bbc
Jackeagle <jackeagle102@gmail.com>
2020-06-02 16:57:59 +0530

    bullhead: Enable AOSP Surfaceflinger Signed-off-by: Jackeagle <jackeagle102@gmail.com>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -34,6 +34,8 @@ TARGET_NO_BOOTLOADER := true
 WITH_DEXPREOPT := true
 DONT_DEXPREOPT_PREBUILTS := true
 
+TARGET_USE_AOSP_SURFACEFLINGER := true
+
 # Inline kernel building
 #KERNEL_TOOLCHAIN := $(shell pwd)/prebuilts/arm64-gcc/bin
 #KERNEL_TOOLCHAIN_PREFIX := aarch64-elf-
