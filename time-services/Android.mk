a1ca1f2d2868f312b9ca70b38d7bfc2df86e6f85
aviraxp <416810799@qq.com>
2020-06-02 15:12:45 +0530

    Bullhead: Drop time service blobs
    All of a sudden, time_genoff connection will fail and sometimes block unlocking with password, etc.
    Stock libtime_genoff.so will fix it in most cases, but not all.
    Drop time service stack to make this connection fail at start to workaround the problem.
    Time service is broken on 10 anyway so it is a good trade-off.
    
    Also Revert "Bullhead: Build with caf time-services"
    
    This reverts commit 16921371b3ec95fbd3e65042abca0c6790792398.

--- time-services/Android.mk
+++ time-services/Android.mk
@@ -5,9 +5,9 @@
 
 LOCAL_PATH:= $(call my-dir)
 
-#include $(CLEAR_VARS)
-#
-#LOCAL_COPY_HEADERS_TO := time-services
-#LOCAL_COPY_HEADERS := ./time_genoff.h
-#
-#include $(BUILD_COPY_HEADERS)
+include $(CLEAR_VARS)
+
+LOCAL_COPY_HEADERS_TO := time-services
+LOCAL_COPY_HEADERS := ./time_genoff.h
+
+include $(BUILD_COPY_HEADERS)
