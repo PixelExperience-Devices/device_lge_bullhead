4b4d7ed60ef0fbbb9c5f5a1a42b24bc51bc9b897
Lakshay Garg <lakshaygarg.in@gmail.com>
2020-06-26 19:51:34 +0530

    Revert "camera phix maibi"
    This reverts commit eaaf6b65500694eba8ca8acf3c85f174ca4e037a.

--- device.mk
+++ device.mk
@@ -307,7 +307,12 @@ PRODUCT_PACKAGES += \
     mm-qcamera-app \
     android.hardware.camera.provider@2.4-impl \
     android.hardware.camera.provider@2.4-service \
+    camera.device@1.0-impl \
     camera.device@3.2-impl
+
+PRODUCT_PACKAGES += \
+    vendor.qti.hardware.camera.device@1.0 \
+    vendor.qti.hardware.camera.device@1.0_vendor
 
 # IMS
 PRODUCT_PACKAGES += \
